9dcbb2d9477a017d23d1fc9957ab6d139fd9eb4f
Proniaiev Maksym <m.pronyayev@beter.co>
2022-10-30 21:18:27 +0200

    v1

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import fetch from 'node-fetch';
 import convert from 'xml-js';
+import fs from 'fs';
+// run windows -> $env:videoId='d76WWAD99Yo'; node ./index.js
 
-fetch('https://youtubetranscript.com/?server_vid=d76WWAD99Yo').then(resp => {
+const origin = 'https://youtubetranscript.com';
+const referrer = (videoId) => `${origin}/?v=${videoId}`;
+const getTranscriptUrl = (videoId) => `${origin}/?server_vid=${videoId}`;
+const getFile = (text, videoId) => {
+    const dir = './texts'
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    fs.writeFile(`${dir}/${videoId}.txt`, text, (err) => {
+        if (err) console.log(err);
+        console.log('Done...');
+    });
+};
+
+const videoId = process.env.videoId;
+
+fetch(getTranscriptUrl(videoId), { headers: { 'referer': referrer(videoId) }}).then(resp => {
     resp.text().then(xml => {
-        var data = convert.xml2js(xml);
-        var text = data.elements.find(el => el.name == 'transcript').elements.map(el => el.elements.map(el => el.text)).join(' ');
-        console.log(text);
+        const data = convert.xml2js(xml)?.elements;
+        const transcript = data.find(el => el.name === 'transcript')?.elements;
+        const text = transcript.map(el => el.elements.map(el => el.text)).join(' ');
+        getFile(text, videoId);
     })
-    
 });
